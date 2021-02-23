29be134b6a01126ae3a361f6a6cf1015a5adba4a
Ky Capstick <ky.capstick@gmail.com>
2021-02-23 10:31:14 -0500

    113 reformting home.vue

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,45 +1,45 @@
 <template>
-  <div>
-    <h1>Where They To</h1>
-    <h2>On Tonight</h2>
-    <h2>This Week</h2>
-  </div>
+    <div>
+        <h1>Where They To</h1>
+        <h2>On Tonight</h2>
+        <h2>This Week</h2>
+    </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-  export default {
+export default {
     data() {
-      return {
-        todays: {},
-        weeks: [],
-      }
+        return {
+            todays: {},
+            weeks: [],
+        }
     },
     computed: {
-      ...mapState(['user']),
+        ...mapState(['user']),
     },
     mounted() {
-      const date = new Date();
-      const today = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
-      this.$store.dispatch('fetchDate', {
-        parameter: 'date',
-        date: today,
-      }).then((response) => {
-        this.todays = JSON.parse(response.data.events);
-      });
-      const tempArray = [];
-      for (let i = 1; i < 7; i = i + 1) {
-        const thisWeeks = {};
-        let weekdate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate() + i}`;
+        const date = new Date();
+        const today = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
         this.$store.dispatch('fetchDate', {
-          parameter: 'date',
-          date: weekdate,
+            parameter: 'date',
+            date: today,
         }).then((response) => {
-          thisWeeks[response.data.date] = JSON.parse(response.data.events);
-          tempArray.push(thisWeeks);
+            this.todays = JSON.parse(response.data.events);
         });
-      }
-      this.weeks = tempArray;
+        const tempArray = [];
+        for (let i = 1; i < 7; i = i + 1) {
+            const thisWeeks = {};
+            let weekdate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate() + i}`;
+            this.$store.dispatch('fetchDate', {
+                parameter: 'date',
+                date: weekdate,
+            }).then((response) => {
+                thisWeeks[response.data.date] = JSON.parse(response.data.events);
+                tempArray.push(thisWeeks);
+            });
+        }
+        this.weeks = tempArray;
     }
-  }
+}
 </script>
