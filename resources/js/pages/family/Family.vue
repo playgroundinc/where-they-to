a517febc7a5854e526ac9b8429fcc66638b6fd6d
Ky Capstick <ky.capstick@gmail.com>
2021-02-24 12:46:42 -0500

    116 add upcoming events to families

--- resources/js/pages/family/Family.vue
+++ resources/js/pages/family/Family.vue
@@ -7,10 +7,10 @@
 				<div class="col-xxs-12 col-md-6">
 				</div>
 				<div class="col-xxs-12 col-md-6">
-          <Button 
-            :label="followinglabel"
-            v-on:clicked.prevent="toggleFollowing"
-          />
+                    <Button 
+                        :label="followinglabel"
+                        v-on:clicked.prevent="toggleFollowing"
+                    />
 					<p>{{ family.description }}</p>
 					<div>
 						<h2>Family Members</h2>
@@ -19,19 +19,27 @@
 								<a :href="'/performers/' + performer.id " v-text="performer.name"></a>
 							</li>
 						</ul>
-            <ul v-if="family.performers_no_profile && family.performers_no_profile.length">
-              <li v-for="performer in family.performers_no_profile" v-bind:key="performer.name">{{ performer.name }}</li>
-            </ul>
+                        <ul v-if="family.performers_no_profile && family.performers_no_profile.length">
+                            <li v-for="performer in family.performers_no_profile" v-bind:key="performer.name">{{ performer.name }}</li>
+                        </ul>
 					</div>
 					<div v-if="socialLinks">
 						<SocialLinks
 							:socialLinks="socialLinks"
 						/>
 					</div>
-          <Updates 
-            type="family"
-            :id="id"
-          />
+                    <UpcomingEvents 
+                        :events="events.current"
+                        :id="id"
+                        :page="events.page"
+                        :total="events.total"
+                        route="families"
+                        v-on:update="updateValue"
+                    />
+                    <Updates 
+                        type="family"
+                        :id="id"
+                    />
 					<div class="copy--center" v-if="familyMember">
 						<Button :link="'/families/' + family.id + '/edit'" label="Edit Family" />
 					</div>
@@ -49,6 +57,7 @@ import { mapState } from 'vuex';
 import SocialLinks from "../../components/SocialLinks";
 import Button from "../../components/Button";
 import Updates from "../../components/Updates";
+import UpcomingEvents from "../../components/UpcomingEvents";
 
 export default {
 
@@ -58,6 +67,7 @@ export default {
 			family: '',
 			socialLinks: [],
 			performers: [],
+            events: {},
 
 		}
     },
@@ -72,10 +82,10 @@ export default {
     followinglabel: function() {
         const families = this.user.following_families;
         if (!families|| !families.length || families.indexOf(this.id) === -1) {
-          return 'Follow';
+            return 'Follow';
         }
         return 'Unfollow'; 
-      }
+        }
 	},
 	methods: {
 		setStates: function(fields, object) {
@@ -90,26 +100,32 @@ export default {
 		getFamily: async function() {
 			const resp = await this.$store.dispatch('fetchSingle', { route: "families", id: this.id });
 			if (resp.status === 200) {
-				const fields = ['family', 'socialLinks', 'performers' ];
+				const fields = ['family', 'socialLinks', 'performers', 'events' ];
 				this.setStates(fields, resp.data);
 			}
-    },
-    async toggleFollowing() {
-      const data = {
-        user_id: this.user.id,
-        route: 'follow/family',
-        route_id: this.id,
-      }
-      const resp = await this.$store.dispatch('toggleEngagement', data)
-      if (resp.data.status && resp.data.status === 'success') {
-        this.$store.dispatch('findUser');
-      }
-    },
+        },
+        async toggleFollowing() {
+            const data = {
+                user_id: this.user.id,
+                route: 'follow/family',
+                route_id: this.id,
+            }
+            const resp = await this.$store.dispatch('toggleEngagement', data)
+            if (resp.data.status && resp.data.status === 'success') {
+                this.$store.dispatch('findUser');
+            } 
+        },
+        updateValue: function(updateObject) {
+            if (this[updateObject.name]) {
+                this[updateObject.name] = updateObject.value;
+            }
+        }
 	},
 	components: {
-    Button,
-    SocialLinks,
-    Updates,
+        Button,
+        SocialLinks,
+        Updates,
+        UpcomingEvents,
 	},
     mounted() {
 		this.getFamily();
